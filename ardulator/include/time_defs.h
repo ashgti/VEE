3dd432054837d638effa117a610c7af293a7c8f5
John Harrison <ash@greaterthaninfinity.com>
2011-02-08 00:02:55 -0600

    Fixed a linux issue

--- ardulator/include/time_defs.h
+++ ardulator/include/time_defs.h
@@ -4,15 +4,6 @@
 extern "C" {
 #endif
 
-typedef enum {
-	CLOCK_REALTIME,
-	CLOCK_MONOTONIC,
-	CLOCK_PROCESS_CPUTIME_ID,
-	CLOCK_THREAD_CPUTIME_ID
-} clockid_t;
-
-int clock_gettime(clockid_t clk_id, struct timespec *tp);
-
 #ifdef __cplusplus
 }
 #endif
