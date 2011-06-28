a545256bdceeb0ebbd43ddaebf563783c748a4ca
John Harrison <ash@greaterthaninfinity.com>
2011-06-28 18:36:06 -0500

    Removing some old files that I dont need, and adding my new driver programs.

---
+++ driver.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+import Foreign
+import Foreign.C.Types
+import System.Posix.Unistd
+import Control.Monad
+
+-- foreign import ccall "arduino.h foo"
+--     c_foo :: CInt
+
+z :: Int -> Int
+z y = if y > 1 then break y else x $ y - 1
+
+x :: Int -> Int
+x y = if y > 4 then y else z $ y + 2
+
+main = do
+    putStrLn $ show $ x 0
+    putStrLn "Done"
+
---
+++ driver.py
@@ -0,0 +1,9 @@
+import vee
+
+def main():
+    a = vee.Arduino()
+    print 'yo'
+    print a.run()
+
+if __name__ == '__main__':
+    main()
