4dd4214fe012dfc952aac8e4f95f4e5be535c57d
John Harrison <ash@greaterthaninfinity.com>
2011-11-01 01:44:55 -0500

    Adding in the doxygen for this project

---
+++ navtree.js
@@ -0,0 +1,362 @@
+var NAVTREE =
+[
+  [ "Virtual Embedded Enviorment", "index.html", [
+    [ "Virtual Embedded Enviorment", "index.html", [
+      [ "About the Project", "_page__about_the_project.html", null ],
+      [ "Architecture", "_page__architecture.html", null ],
+      [ "Arduino API and Control Interface", "_page__arduino_a_p_i.html", null ],
+      [ "Student Guide", "_page__student_guide.html", null ],
+      [ "Teachers Guide", "_page__teacher_guide.html", null ],
+      [ "Extending the Emulation", "_page__extending.html", null ]
+    ] ],
+    [ "Related Pages", "pages.html", [
+      [ "Todo List", "todo.html", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Analog", "classvee_1_1signals_1_1_analog.html", null ],
+      [ "ApplicationRunner", "classvee-gui_1_1_application_runner.html", null ],
+      [ "ArduException", "classardulator_1_1_ardu_exception.html", null ],
+      [ "Ardulator", "classardulator_1_1_ardulator.html", null ],
+      [ "BitValue", "classardulator_1_1containers_1_1_bit_value.html", null ],
+      [ "Clock", "structardulator_1_1containers_1_1_clock.html", null ],
+      [ "ConfigurationError", "classvee-gui_1_1_configuration_error.html", null ],
+      [ "Digital", "classvee_1_1signals_1_1_digital.html", null ],
+      [ "EmulatorFinished", "classardulator_1_1_emulator_finished.html", null ],
+      [ "Exp", "classvee_1_1signals_1_1_exp.html", null ],
+      [ "HardwareSerial", "class_hardware_serial.html", null ],
+      [ "History", "struct_history.html", null ],
+      [ "MainWindow", "classvee-gui_1_1_main_window.html", null ],
+      [ "PinConfig", "structardulator_1_1_pin_config.html", null ],
+      [ "Print", "class_print.html", null ],
+      [ "ProcessingSignalException", "classardulator_1_1_processing_signal_exception.html", null ],
+      [ "PyArdulator", "classvee_1_1ardulator_1_1_py_ardulator.html", null ],
+      [ "PySignalImp", "classvee_1_1ardulator_1_1_py_signal_imp.html", null ],
+      [ "PyValueImp", "classvee_1_1ardulator_1_1_py_value_imp.html", null ],
+      [ "Serial", "classvee_1_1signals_1_1_serial.html", null ],
+      [ "Signal", "classvee_1_1signals_1_1_signal.html", null ],
+      [ "SignalContainer", "structardulator_1_1containers_1_1_signal_container.html", null ],
+      [ "SignalImp", "struct_signal_imp.html", null ],
+      [ "SineWave", "classvee_1_1signals_1_1_sine_wave.html", null ],
+      [ "SquareWave", "classvee_1_1signals_1_1_square_wave.html", null ],
+      [ "Uni", "classvee_1_1signals_1_1_uni.html", null ],
+      [ "ValueImp", "union_value_imp.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "Analog", "classvee_1_1signals_1_1_analog.html", null ],
+      [ "ApplicationRunner", "classvee-gui_1_1_application_runner.html", null ],
+      [ "ArduException", "classardulator_1_1_ardu_exception.html", null ],
+      [ "Ardulator", "classardulator_1_1_ardulator.html", null ],
+      [ "BitValue", "classardulator_1_1containers_1_1_bit_value.html", null ],
+      [ "Clock", "structardulator_1_1containers_1_1_clock.html", null ],
+      [ "ConfigurationError", "classvee-gui_1_1_configuration_error.html", null ],
+      [ "Digital", "classvee_1_1signals_1_1_digital.html", null ],
+      [ "EmulatorFinished", "classardulator_1_1_emulator_finished.html", null ],
+      [ "Exp", "classvee_1_1signals_1_1_exp.html", null ],
+      [ "History", "struct_history.html", null ],
+      [ "MainWindow", "classvee-gui_1_1_main_window.html", null ],
+      [ "PinConfig", "structardulator_1_1_pin_config.html", null ],
+      [ "Print", "class_print.html", [
+        [ "HardwareSerial", "class_hardware_serial.html", null ]
+      ] ],
+      [ "ProcessingSignalException", "classardulator_1_1_processing_signal_exception.html", null ],
+      [ "PyArdulator", "classvee_1_1ardulator_1_1_py_ardulator.html", null ],
+      [ "PySignalImp", "classvee_1_1ardulator_1_1_py_signal_imp.html", null ],
+      [ "PyValueImp", "classvee_1_1ardulator_1_1_py_value_imp.html", null ],
+      [ "Serial", "classvee_1_1signals_1_1_serial.html", null ],
+      [ "Signal", "classvee_1_1signals_1_1_signal.html", null ],
+      [ "SignalContainer", "structardulator_1_1containers_1_1_signal_container.html", null ],
+      [ "SignalImp", "struct_signal_imp.html", null ],
+      [ "SineWave", "classvee_1_1signals_1_1_sine_wave.html", null ],
+      [ "SquareWave", "classvee_1_1signals_1_1_square_wave.html", null ],
+      [ "Uni", "classvee_1_1signals_1_1_uni.html", null ],
+      [ "ValueImp", "union_value_imp.html", null ]
+    ] ],
+    [ "Data Fields", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "ardulator", "namespaceardulator.html", null ],
+      [ "ardulator::containers", "namespaceardulator_1_1containers.html", null ],
+      [ "vee", "namespacevee.html", null ],
+      [ "vee-cmd", "namespacevee-cmd.html", null ],
+      [ "vee-gui", "namespacevee-gui.html", null ],
+      [ "vee::ardulator", "namespacevee_1_1ardulator.html", null ],
+      [ "vee::signals", "namespacevee_1_1signals.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "include/arduino.h", "arduino_8h.html", null ],
+      [ "include/ardulator.h", "ardulator_8h.html", null ],
+      [ "include/pins_arduino.h", "pins__arduino_8h.html", null ],
+      [ "include/ardulator/cdefs.h", "cdefs_8h.html", null ],
+      [ "include/ardulator/pin_config.h", "pin__config_8h.html", null ],
+      [ "include/ardulator/containers/bit_value.h", "bit__value_8h.html", null ],
+      [ "include/ardulator/containers/clock.h", "clock_8h.html", null ],
+      [ "include/ardulator/containers/history.h", "history_8h.html", null ],
+      [ "include/ardulator/containers/signals.h", "signals_8h.html", null ],
+      [ "include/avr/config.h", "config_8h.html", null ],
+      [ "include/avr/interrupt.h", "interrupt_8h.html", null ],
+      [ "src/ardulator.cc", "ardulator_8cc.html", null ],
+      [ "src/emulator_commands.cc", "emulator__commands_8cc.html", null ],
+      [ "src/interrupt_commands.cc", "interrupt__commands_8cc.html", null ],
+      [ "src/pin_commands.cc", "pin__commands_8cc.html", null ],
+      [ "src/time_commands.cc", "time__commands_8cc.html", null ],
+      [ "src/vee-cmd.py", "vee-cmd_8py.html", null ],
+      [ "src/vee-gui.py", "vee-gui_8py.html", null ],
+      [ "src/ardulator/hardware_serial.cc", "hardware__serial_8cc.html", null ],
+      [ "src/ardulator/pin_config.cc", "pin__config_8cc.html", null ],
+      [ "src/ardulator/containers/bit_value.cc", "bit__value_8cc.html", null ],
+      [ "src/c_interface/env.cc", "env_8cc.html", null ],
+      [ "src/c_interface/interface.cc", "interface_8cc.html", null ],
+      [ "src/vee/__init__.py", "____init_____8py.html", null ],
+      [ "src/vee/ardulator.py", "ardulator_8py.html", null ],
+      [ "src/vee/signals.py", "signals_8py.html", null ]
+    ] ],
+    [ "Globals", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
