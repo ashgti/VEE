262a88fa52caca4497de62b4a2092340db1310a0
John Harrison <ash@greaterthaninfinity.com>
2011-11-08 01:59:44 -0600

    Finished with the documentation?

--- docs/update_names.sh
+++ docs/update_names.sh
@@ -1,3 +1,5 @@
 find . -name '_page*' -type f -print | sed 's/\.\/_page\(.*\)/mv \.\/_page\1 \.\/page\1/' | sh
-
 find . -type f -print0 | xargs -0 -n 1 sed -i -e 's/_page/page/g'
+
+find . -name '____init*' -type f -print | sed 's/\.\/____init\(.*\)/mv \.\/____init\1 \.\/init\1/' | sh
+find . -type f -print0 | xargs -0 -n 1 sed -i -e 's/____init/init/g'
